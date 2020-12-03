fd62e9584852de72365907cef59a089d45916309
Irfan Fauzan Rachman <irfanfauzanrachman@gmail.com>
2020-12-03 22:16:23 +0700

    Initial Commit

---
+++ Games List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Games List
+//
+//  Created by Irfan Fauzan R on 03/12/20.
+//  Copyright © 2020 Irfan Fauzan R. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
